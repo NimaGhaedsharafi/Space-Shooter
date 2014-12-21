bba762fef058a67e254e5f357f8778a0fa8ddf73
Ghaedsharafi, Nima <ghaedsharafi.nima@gmail.com>
2014-12-21 16:15:13 +0330

    EP5
    Signed-off-by: Ghaedsharafi, Nima <ghaedsharafi.nima@gmail.com>

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+
+	public GameObject explosionHazzard;
+	public GameObject explosionPlayer;
+	
+
+	void OnTriggerEnter(Collider other) {
+
+		if (other.CompareTag ("hazzard"))
+		{
+			Debug.Log ("Hazzard detected");
+			Instantiate(explosionHazzard, transform.position, Quaternion.identity);
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+		else if(other.CompareTag("Player"))
+		{
+			Debug.Log ("Player Detected");
+			Instantiate(explosionHazzard, transform.position, Quaternion.identity);
+			Instantiate(explosionPlayer, transform.position, Quaternion.identity);
+			
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+		else if(other.CompareTag("shoot"))
+		{
+			Debug.Log ("Shoot Detected");
+			Instantiate(explosionHazzard, transform.position, Quaternion.identity);
+			Destroy(other.gameObject);
+			Destroy(gameObject);
+		}
+
+	}
+}
---
+++ Assets/Scripts/RandomRototor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRototor : MonoBehaviour {
+
+	public float tumbleFact;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 randomPoint = new Vector3 (Random.value, Random.value, Random.value);
+		this.rigidbody.angularVelocity = randomPoint * tumbleFact;
+	}
+}

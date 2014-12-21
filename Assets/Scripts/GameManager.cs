8648e35e134234f27f1f0fa6bf14b5663a030ea1
Ghaedsharafi, Nima <ghaedsharafi.nima@gmail.com>
2014-12-21 16:21:08 +0330

    EP6
    Signed-off-by: Ghaedsharafi, Nima <ghaedsharafi.nima@gmail.com>

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	int hazzardCount = 5;
+	float hazzardWait = .5f;
+	float WaveWait = 1f;
+	float ReadyWait = 1f;
+	
+
+	float x = 7;
+
+	// go astroids
+	public GameObject hazzard;
+
+	// go hazzardSpawn
+	public Transform hazzardSpwan;
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine(hazzardWave());
+
+	}
+	
+
+	IEnumerator hazzardWave()
+	{
+		yield return new WaitForSeconds(ReadyWait);
+		while(true)
+		{
+			for(int i =0; i < hazzardCount; i++) 
+			{
+				Instantiate (hazzard, new Vector3 (Random.Range(x, -x), hazzardSpwan.position.y, hazzardSpwan.position.z), Quaternion.identity);
+				yield return new WaitForSeconds(hazzardWait);
+			}
+			yield return new WaitForSeconds(WaveWait);
+
+		}
+
+	}
+}
